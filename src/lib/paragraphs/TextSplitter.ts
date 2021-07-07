f474325fc48213dd34bba7148a5b0ee902f40130
Semen <bethesda.stillxxxx@gmail.com>
2021-07-07 13:37:26 +0300

    ENG: Final fix of text splitter\nRU: Финальный фикс парсера текста

--- src/lib/paragraphs/TextSplitter.ts
+++ src/lib/paragraphs/TextSplitter.ts
@@ -6,6 +6,57 @@ interface SplitParams {
     orderStartingFrom: number
 }
 
+
+/*
+Функция, которая будет переносить лишние предложения в другие
+
+ splitBySentence:
+- false, когда нужно убрать остатки предложений, которые начались в одной строке, а кончились в другой
+- true, если нужно делить предложения только по разделителям
+ Предложения в строках при этом не разорваны
+*/
+function moveOddSentencesToNext(strings: string[], params: SplitParams, i: number, splitBySentences = false) {
+    if (strings[i].length == 0) {
+        return;
+    }
+
+    let delimPosition = -1
+    // Ищем ближайший к концу разделитель
+    for (let j = 0; j < params.paragraphDelimits.length; j++) {
+        let delimiter = params.paragraphDelimits[j]
+        let currentDelimPosition: number
+
+        if (splitBySentences) {
+            currentDelimPosition = strings[i].lastIndexOf(delimiter, strings[i].length - 2)
+        }
+        else {
+            currentDelimPosition = strings[i].lastIndexOf(delimiter)
+        }
+
+
+        if (currentDelimPosition > delimPosition) {
+            delimPosition = currentDelimPosition
+        }
+    }
+
+    // Если после разделителя есть пробел
+    // то есть это не конец строки
+    if (strings[i][delimPosition + 1] == ' ') {
+        delimPosition += 1
+    }
+
+    // Выделяем подстроку и переносим в следующий элемент
+    if (strings[i+1]) {
+        strings[i + 1] = strings[i].substr(delimPosition + 1) + strings[i + 1]
+    }
+    else {
+        strings.push(strings[i].substr(delimPosition + 1))
+    }
+
+    // Убираем из текущей строки выделенную
+    strings[i] = strings[i].substring(0, delimPosition)
+}
+
 export default function splitTextToParagraphs(text: string, params: SplitParams): Paragraphs[]  {
     let strings: string[] = []
 
@@ -32,77 +83,24 @@ export default function splitTextToParagraphs(text: string, params: SplitParams)
     let  length = strings.length
     // Начинаем убирать из каждой строки
     // остатки предложений, чтобы не было разрывов и неправильных переходов между строками.
-    // Идем от начала до конца, из начала лишние предложения будут переходить в конечные строки
+    // Идем от начала до конца, из начала лишние части будут переходить в конечные строки
     for (let i = 0; i < length; i++) {
-        let delimPosition = -1
-        // Ищем ближайший к концу разделитель
-        for (let j = 0; j < params.paragraphDelimits.length; j++) {
-            let delimiter = params.paragraphDelimits[j]
-            let currentDelimPosition = strings[i].lastIndexOf(delimiter)
-
-            if (currentDelimPosition > delimPosition) {
-                delimPosition = currentDelimPosition
-            }
-        }
-
-        // Если после разделителя есть пробел
-        // то есть это не конец строки
-        if (strings[i][delimPosition + 1] == ' ') {
-            delimPosition += 1
-        }
-
-        // Выделяем подстроку и переносим в следующий элемент
-        if (strings[i+1]) {
-            strings[i + 1] = strings[i].substr(delimPosition + 1) + strings[i + 1]
-        }
-        else {
-            strings.push(strings[i].substr(delimPosition + 1))
-        }
-
-        // Убираем из текущей строки выделенную
-        strings[i] = strings[i].substring(0, delimPosition)
+        moveOddSentencesToNext(strings, params, i)
     }
-
-    // Убираем пустые элементы
-    strings = strings.filter(v => {
-        return v.length != 0;
-    })
 
     // После всего этого, могут остаться слишком большие строки
     // Чтобы убрать их, будем постепенно лишние предложения перемещать в последние строки
     length = strings.length
     for (let i = 0; i < length; i++) {
         while (strings[i].length > params.maxSymbolsCount) {
-
-            let delimPosition = -1
-            // Ищем ближайший к концу разделитель
-            for (let j = 0; j < params.paragraphDelimits.length; j++) {
-                let delimiter = params.paragraphDelimits[j]
-                let currentDelimPosition = strings[i].lastIndexOf(delimiter, strings[i].length - 2)
-
-                if (currentDelimPosition > delimPosition) {
-                    delimPosition = currentDelimPosition
-                }
-            }
-
-            // Если после разделителя есть пробел
-            // то есть это не конец строки
-            if (strings[i][delimPosition + 1] == ' ') {
-                delimPosition += 1
-            }
-
-            // Выделяем подстроку и переносим в следующий элемент
-            if (strings[i+1]) {
-                strings[i + 1] = strings[i].substr(delimPosition + 1) + strings[i + 1]
-            }
-            else {
-                strings.push(strings[i].substr(delimPosition + 1))
-            }
-
-            // Убираем из текущей строки выделенную
-            strings[i] = strings[i].substring(0, delimPosition)
+            moveOddSentencesToNext(strings, params, i, true)
         }
     }
+
+    // Убираем пустые элементы
+    strings = strings.filter(v => {
+        return v.length != 0;
+    })
 
     let order = params.orderStartingFrom - 1
     return strings.map(v => {
